c4f29c904c6a61c16f663b6807794d4a69b7d40b
Lauther Valladares <lautherharold27@gmail.com>
2022-11-23 20:57:03 -0500

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,15 @@ import '../assets/style/footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <div className='footer'>footer</div>
-        </div>
+        <footer className='footer'>
+            <h3>By: Lauther Valladares</h3>
+            <h2>Made with <i className="fa-solid fa-heart fa-beat"></i> in Academlo</h2>
+            <div className='social-icons fa-bounce'>
+                <i className="fa-brands fa-linkedin fa-xl"></i>
+                <i className="fa-brands fa-github fa-xl"></i>
+                <i className="fa-brands fa-instagram fa-xl"></i>
+            </div>
+        </footer>
     );
 };
 
